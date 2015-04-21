9e4d1faf8f1186225b445879e89505e6dee4cba1
pwtyler <philip@tylerdigital.com>
2015-04-21 16:15:06 -0700

    Added TGM-Plugin-Activation 2.4.0
    library checks for Tablepress, asks to install/activate if not found

--- advanced-custom-fields-tablepress.php
+++ advanced-custom-fields-tablepress.php
@@ -4,6 +4,8 @@ Plugin Name: Advanced Custom Fields: TablePress
 Description: ACF field to select one or many TablePress Tables
 Version: 1.0.0
 Author: @pwtyler of @tylerdigital
+Text Domain: advanced-custom-fields-tablepress
+
 */
 
 // $version = 5 and can be ignored until ACF6 exists
@@ -13,18 +15,62 @@ function include_field_types_TablePress( $version ) {
 
 add_action( 'acf/include_field_types', 'include_field_types_tablepress' );
 
-/* Added to check if TablePress is installed on activation. */
-function tp_activate() {
+require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 
-	if ( defined( 'TABLEPRESS_ABSPATH' ) ) {
-		return true;
-	}
+add_action( 'tgmpa_register', 'acftp_register_required_plugins' );
+function acftp_register_required_plugins() {
 
-	$html = '<div class="error">';
-	$html .= '<p>';
-	$html .= _e( 'Warning: TablePress is not installed or activated. This plugin does not function without TablePress!' );
-	$html .= '</p>';
-	$html .= '</div>';
-	echo $html;
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name'      => 'Tablepress',
+			'slug'      => 'tablepress',
+			'required'  => true,
+		),
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 * If you want the default strings to be available under your own theme domain,
+	 * leave the strings uncommented.
+	 * Some of the strings are added into a sprintf, so see the comments at the
+	 * end of each line for what each argument will be.
+	 */
+	$config = array(
+		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+		'strings'      => array(
+			'page_title'                      => __( 'Install Required Plugins', 'advanced-custom-fields-tablepress' ),
+			'menu_title'                      => __( 'Install Plugins', 'advanced-custom-fields-tablepress' ),
+			'installing'                      => __( 'Installing Plugin: %s', 'advanced-custom-fields-tablepress' ), // %s = plugin name.
+			'oops'                            => __( 'Something went wrong with the plugin API.', 'advanced-custom-fields-tablepress' ),
+			'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'advanced-custom-fields-tablepress' ), // %1$s = plugin name(s).
+			'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'advanced-custom-fields-tablepress' ),
+			'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'advanced-custom-fields-tablepress' ),
+			'return'                          => __( 'Return to Required Plugins Installer', 'advanced-custom-fields-tablepress' ),
+			'plugin_activated'                => __( 'Plugin activated successfully.', 'advanced-custom-fields-tablepress' ),
+			'complete'                        => __( 'All plugins installed and activated successfully. %s', 'advanced-custom-fields-tablepress' ), // %s = dashboard link.
+			'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+		)
+	);
+
+	tgmpa( $plugins, $config );
 }
-register_activation_hook( __FILE__, 'tp_activate' );
