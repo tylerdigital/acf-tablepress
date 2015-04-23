68c9f1d1c8588423fa1ff297f712b8b40d4719c9
pwtyler <philip@tylerdigital.com>
2015-04-23 16:46:04 -0700

    Removed boilerplate comments

--- tablepress-v5.php
+++ tablepress-v5.php
@@ -1,57 +1,18 @@
 <?php
-
 class acf_field_tablepress extends acf_field {
   
-  /*
-  *  __construct
-  *
-  *  This function will setup the field type data
-  *
-  *  @type  function
-  *  @date  5/03/2014
-  *  @since 5.0.0
-  *
-  *  @param n/a
-  *  @return  n/a
-  */
-  
   function __construct() {
-    // vars
     $this->name = 'tablepress_field';
     $this->label = __('TablePress');
-    $this->category = __("Relational",'acf'); // Basic, Content, Choice, etc
+    $this->category = __("Relational",'acf');
     $this->defaults = array(
       'allow_null' => 0
     );
 
-    // do not delete!
     parent::__construct();
   }
   
-  /*
-  *  render_field_settings()
-  *
-  *  Create extra settings for your field. These are visible when editing a field
-  *
-  *  @type  action
-  *  @since 3.6
-  *  @date  23/01/13
-  *
-  *  @param $field (array) the $field being edited
-  *  @return  n/a
-  */
-  
   function render_field_settings( $field ) {
-    
-    /*
-    *  acf_render_field_setting
-    *
-    *  This function will create a setting for your field. Simply pass the $field parameter and an array of field settings.
-    *  The array of settings does not require a `value` or `prefix`; These settings are found from the $field array.
-    *
-    *  More than one setting can be added by copy/paste the above code.
-    *  Please note that you must also have a matching $defaults value for the field name (font_size)
-    */
     
     acf_render_field_setting( $field, array(
       'label' => 'Allow Null?',
@@ -66,22 +27,8 @@ class acf_field_tablepress extends acf_field {
 
   }
   
-  /*
-  *  render_field()
-  *
-  *  Create the HTML interface for your field
-  *
-  *  @param $field (array) the $field being rendered
-  *
-  *  @type  action
-  *  @since 3.6
-  *  @date  23/01/13
-  *
-  *  @param $field (array) the $field being edited
-  *  @return  n/a
-  */
-  
   function render_field( $field ) {
+    /* Exits function if TablePress not active */
     if ( !defined( 'TABLEPRESS_ABSPATH' ) ) {
       echo __('TablePress must be activated for this ACF field to work', 'advanced-custom-fields-tablepress');
       return;
@@ -129,5 +76,4 @@ class acf_field_tablepress extends acf_field {
   }
 }
 
-// create field
 new acf_field_tablepress();
